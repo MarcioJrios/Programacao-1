70b96cc5ac2a8a93253d3a2fe7f43df2a6dbb82f
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2018-03-24 02:14:36 +0000

    Update Medicamento.java

--- TrabT1/Medicamento.java
+++ TrabT1/Medicamento.java
@@ -6,15 +6,34 @@ class Medicamento{
   boolean receitaObrigatoria;
   int qtdEstoque;
 
-  reajustarPrecoVenda(double percentual){
+  Medicamento(Laboratorio lab){
+	this.nome = "Dorflex";
+	this.precoVenda = 10;
+	this.dataValidade.dia = 22;
+	this.dataValidade.mes = 3;
+	this.dataValidade.ano = 2020;
+	this.laboratorio = lab;
+	this.receitaObrigatoria = false;
+	this.qtdEstoque = 12;
+  }
+
+  void reajustarPrecoVenda(double percentual){
+	percentual = percentual / 100;
+	this.precoVenda = this.precoVenda * percentual;
+	
+  }
+
+  void vender(int quantidade){
+	this.qtdEstoque = this.qtdEstoque - quantidade;
 
   }
 
-  vender(int quantidade){
+  void comprar(int quantidade){
+	this.qtdEstoque = this.qtdEstoque + quantidade;
+	
 
   }
 
-  comprar(int quantidade){
-
+  void mostra(){
   }
 }
