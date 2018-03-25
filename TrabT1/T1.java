6ea1c5b4a6374ad2835ea3154b52f46405eaeb2f
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2018-03-25 19:30:51 -0300

    Update T1.java

--- TrabT1/T1.java
+++ TrabT1/T1.java
@@ -3,40 +3,52 @@ import java.util.Scanner;
 class T1{
   public static void main(String[] args){
     Laboratorio lab = new Laboratorio();
-    Medicamento A = new Medicamento(lab);
+    Data data = new Data(22, 3, 2020);
+    Medicamento A = new Medicamento(lab, data);
     Scanner teclado = new Scanner(System.in);
-    int n;
+    int n, quantidade;
+    double percentual;
     do{
       System.out.println("Escolha uma opção:");
       System.out.println("1: Reajustar preço de venda");
-      System.out.println("2: Vender");
-      System.out.println("3: Comprar");
+      System.out.println("2: Comprar");
+      System.out.println("3: Vender");
       System.out.println("4: Mostrar data de validade");
       System.out.println("0: Sair");
       n = teclado.nextInt();
       switch(n){
         case 1:
-	  double percentual;
 	  System.out.println("Digite o percentual");
 	  percentual = teclado.nextDouble();
           A.reajustarPrecoVenda(percentual);
           break;
         case 2:
-	  int quantidade2;
-	  quantidade2 = teclado.nextInt();
-          A.comprar(quantidade2);
+	  System.out.println("Digite a quantidade a comprar: ");
+	  quantidade = teclado.nextInt();
+          A.comprar(quantidade);
           break;
         case 3:
-	  int quantidade1;
-	  quantidade1 = teclado.nextInt();
-          A.vender(quantidade1);
+	  if(A.receitaObrigatoria = true){
+	     System.out.println("Este medicamento so pode ser adquirido com uma Receita Obrigatória");
+	     System.out.println("Digite a quantidade a vender: ");
+	     quantidade = teclado.nextInt();
+             A.vender(quantidade);
+		}else{
+		   System.out.println("Digite a quantidade a vender: ");
+		   quantidade = teclado.nextInt();
+		   A.vender(quantidade);
+			}
           break;
         case 4:
+	  A.mostra();
           break;
+	case 0:
+	  break;
         default:
+	  System.out.println("Opção inválida!");
           break;
       }
-    }while(n != 4);
+    }while(n != 0);
 
   }
 }
