65faaa5fa89a523c724d535b169ee35e2446f70b
MarcioJrios <31110984+MarcioJrios@users.noreply.github.com>
2018-03-27 11:02:09 -0300

    Update Empresa.java

--- Empresa.java
+++ Empresa.java
@@ -1,23 +1,99 @@
-import java.util.list;
-import java.util.scanner;
+import java.util.*; 
 
 public class Empresa{
-	
-	String nome;
-	String cnpj;
-	Funcionario[] Bodegueiros = new Funcionario[10];
-	ArrayList Produtos;
-	ArrayList Clientes;
-	int i = 0;
+	String Nome, cnpj, Dono;
+	Scanner entrada = new Scanner(System.in);
+	boolean vrf = false;
 
-public void Contrata(){
-		Bodegueiros[i] = new Funcionario();
-		this.nome = nome;
-		this.cpf = cpf;
-		this.salario = salario;
-		this.cargo = cargo;
+	public Empresa(String Nome, String cnpj, String Dono){
+		this.Dono = Dono;
+		this.Nome = Nome;
+		this.cnpj = cnpj;
 	}
-	
+	public Empresa(){
+
+	}
+
+	public Funcionario contrata(){
+		String nome, cpf, nasc, cargo;
+		double salario;
+		if(vrf)
+			entrada.nextLine();
+		else
+			vrf = true;
+		System.out.print("Digite o nome do bodegueiro: ");
+		nome = entrada.nextLine();
+		System.out.print("\nCPF: ");
+		cpf = entrada.next();
+		System.out.print("\nSalario: ");
+		salario = entrada.nextDouble();
+		System.out.print("\nCargo: ");
+		entrada.nextLine();
+		cargo = entrada.nextLine();
+		System.out.print("\nData de nascimento: ");
+		nasc = entrada.next();
+		Funcionario bo = new Funcionario(nome, cpf, nasc, cargo, salario);
+		return bo;
+	}
+
+	public Bebida cadastraBebida(){
+		if(vrf)
+			entrada.nextLine();
+		else
+			vrf = true;
+		System.out.print("Digite o nome da bebida: ");
+		String nome = entrada.nextLine();
+		System.out.print("\nTeor alcoolico: ");
+		String teor = entrada.next();
+		System.out.print("\nPreço: ");
+		double preco = entrada.nextDouble();
+		System.out.print("\nQuantidade de ml: ");
+		int ml = entrada.nextInt();
+		System.out.print("\nQuantidade de bebidas: ");
+		int qtd = entrada.nextInt();
+		Bebida beb = new Bebida(nome, preco, teor, ml, qtd);
+		return beb;
+	}
+
+	public Cliente cadastraCliente(){
+		String nome, cpf, c;
+		boolean fiado = false;
+		if(vrf)
+			entrada.nextLine();
+		else
+			vrf = true;
+		System.out.print("Digite o nome do Cliente: ");
+		nome = entrada.nextLine();
+		System.out.print("\nCPF: ");
+		cpf = entrada.next();
+		System.out.println("Pode comprar fiado? (S/N)");
+		c = entrada.next();
+		if(c.equals("S"))
+			fiado = true;
+		else if(c.equals("N"))
+			fiado = false;
+		Cliente cl = new Cliente(nome, cpf, fiado);
+		return cl;
+	}
+
+	public void mostraFun(ArrayList<Funcionario> bod){
+		for(int i = 0; i < bod.size(); i++){
+				Funcionario b = bod.get(i);
+			System.out.println("Dados Bodegueiro [" +(i+1)+ "]" + b);
+		}
+	}
+
+	public void mostraBeb(ArrayList<Bebida> beb){
+		for(int i = 0; i < beb.size(); i++){
+			Bebida b = beb.get(i);
+			System.out.println("Dados Bebida [" +(i+1)+"]" + b);
+		}
+	}
+
+	public void mostraCli(ArrayList<Cliente> cli){
+		for(int i = 0; i < cli.size(); i++){
+			Cliente c = cli.get(i);
+			System.out.println("Dados Cliente [" +(i+1)+"]" + c);
+		}
+	}
 }
-
-
